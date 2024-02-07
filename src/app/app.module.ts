e04403831dfbde5cfe6bc497dd3d1557507cb53f
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-02-07 11:31:19 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,47 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    DashboardComponent,
+    DialogAddUserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FormsModule
+    
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,29 @@
+import { Component} from '@angular/core';
+import { User } from '../../app/models/user.class';
+
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate?: Date;
+
+  //firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
+  saveUser() {
+    if(this.birthDate != undefined) {
+      this.user.birthDate = this.birthDate.getTime();
+      console.log('Current user is', this.user);
+
+      this.firestore.collection('users').add(this.user.toJSON()).then((result: any) => {
+        console.log('Adding user finished', result);
+      });
+    }
+  }
+
+}
